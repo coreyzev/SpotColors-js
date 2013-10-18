32b362185e271f428440210d15c580915109c7f1
coreyzev <coreyzev@zevdesigns.com>
2013-10-18 13:01:07 -0400

    linted
    now to reorganize and simplify for js

--- linted.js
+++ linted.js
@@ -32,16 +32,16 @@ function color() {
     "b": 0
   };
   this.pantone = "";
-  this.color = function() {
+  this.color = function () {
     var value = 0, x, y;
     for (x = 0; x < 16; x++) {
       for (y = 0; y < 16; y++) {
-        this.hex_trip[value] = this.hexbase[x] + "" + this.hexbase[y];
+        this.hex_trip[value] = String(this.hexbase[x]) + String(this.hexbase[y]);
         value++;
       }
     }
   };
-  this.set_rgb = function(red, green, blue) {
+  this.set_rgb = function (red, green, blue) {
     this.rgb.red = red;
     this.rgb.green = green;
     this.rgb.blue = blue;
@@ -49,14 +49,14 @@ function color() {
     this.convert_rgb_to_cmyk();
     this.convert_rgb_to_hex();
   };
-  this.set_hex = function(hex) {
+  this.set_hex = function (hex) {
     hex = hex.toString().toLowerCase();
     this.hex = hex;
 
     this.convert_hex_to_rgb();
     this.convert_rgb_to_cmyk();
   };
-  this.set_cmyk = function(c, m, y, b) {
+  this.set_cmyk = function (c, m, y, b) {
     this.cmyk.c = c;
     this.cmyk.m = m;
     this.cmyk.y = y;
@@ -65,7 +65,7 @@ function color() {
     this.convert_cmyk_to_rgb();
     this.convert_rgb_to_hex();
   };
-  this.set_pantone = function(pantone_name) {
+  this.set_pantone = function (pantone_name) {
     this.pantone = pantone_name;
     this.cmyk.c = this.pantone_pallete[pantone_name].c;
     this.cmyk.m = this.pantone_pallete[pantone_name].m;
@@ -75,24 +75,24 @@ function color() {
     this.convert_cmyk_to_rgb();
     this.convert_rgb_to_hex();
   };
-  this.get_rgb = function() {
+  this.get_rgb = function () {
     return this.rgb;
   };
-  this.get_hex = function() {
+  this.get_hex = function () {
     return this.hex;
   };
-  this.get_cmyk = function() {
+  this.get_cmyk = function () {
     return this.cmyk;
   };
-  this.convert_rgb_to_hex = function() {
-    this.hex = this.hex_trip[this.rgb.red] + "" + this.hex_trip[this.rgb.green] + "" + this.hex_trip[this.rgb.blue];
+  this.convert_rgb_to_hex = function () {
+    this.hex = String(this.hex_trip[this.rgb.red]) + String(this.hex_trip[this.rgb.green]) + String(this.hex_trip[this.rgb.blue]);
   };
-  this.convert_rgb_to_cmyk = function() {
-    var c = (round((255 - this.rgb_red) / 255.0 * 100, 0));
-    var m = (round((255 - this.rgb_green) / 255.0 * 100, 0));
-    var y = (round((255 - rgbval + "" + blue) / 255.0 * 100, 0));
+  this.convert_rgb_to_cmyk = function () {
+    var c = (Math.round((255 - this.rgb.red) / 255.0 * 100, 0));
+    var m = (Math.round((255 - this.rgb.green) / 255.0 * 100, 0));
+    var y = (Math.round((255 - this.rgb.blue) / 255.0 * 100, 0));
 
-    var b = min({
+    var b = Math.min({
       0: c,
       1: m,
       2: y
@@ -109,7 +109,7 @@ function color() {
       "b": b
     };
   };
-  this.convert_cmyk_to_rgb = function() {
+  this.convert_cmyk_to_rgb = function () {
     var red = this.cmyk.c + this.cmyk.b;
     var green = this.cmyk.m + this.cmyk.b;
     var blue = this.cmyk.y + this.cmyk.b;
@@ -118,41 +118,41 @@ function color() {
     green = (green - 100) * (-1);
     blue = (blue - 100) * (-1);
 
-    red = round(red / 100 * 255, 0);
-    green = round(green / 100 * 255, 0);
-    blue = round(blue / 100 * 255, 0);
+    red = Math.round(red / 100 * 255, 0);
+    green = Math.round(green / 100 * 255, 0);
+    blue = Math.round(blue / 100 * 255, 0);
 
     this.rgb.red = red;
     this.rgb.green = green;
     this.rgb.blue = blue;
   };
-  this.convert_hex_to_rgb = function() {
-    var red = substr(this.hex, 0, 2);
-    var green = substr(this.hex, 2, 2);
-    var blue = substr(this.hex, 4, 2);
+  this.convert_hex_to_rgb = function () {
+    var red = this.hex.substr(0, 2);
+    var green = this.hex.substr(2, 2);
+    var blue = this.hex.substr(4, 2);
 
-    var found = false;
-    for (var i = 0; i < count(this.hex_trip) && !found; i++) {
-      if (this.hex_trip[i] == red) {
+    var found = false, i;
+    for (i = 0; i < this.hex_trip.length() && !found; i++) {
+      if (this.hex_trip[i] === red) {
         this.rgb.red = i;
         found = true;
       }
     }
 
     found = false;
-    for (i = 0; i < count(this.hex_trip) && !found; i++) {
-      if (this.hex_trip[i] == green) {
+    for (i = 0; i < this.hex_trip.length() && !found; i++) {
+      if (this.hex_trip[i] === green) {
         this.rgb.green = i;
         found = true;
       }
     }
 
     found = false;
-    for (i = 0; i < count(this.hex_trip) && !found; i++) {
-      if (this.hex_trip[i] == blue) {
+    for (i = 0; i < this.hex_trip.length() && !found; i++) {
+      if (this.hex_trip[i] === blue) {
         this.rgb.blue = i;
         found = true;
       }
     }
   };
-};
+}

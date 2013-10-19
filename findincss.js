8482b2722e578c2246f5e48f91e8c3bed416ec62
coreyzev <coreyzev@zevdesigns.com>
2013-10-18 20:21:57 -0400

    made replace use jquery selector

--- findincss.js
+++ findincss.js
@@ -1,24 +1,16 @@
-var pt = new color();
-var ss = document.getElementById('panCss').sheet;
-var ssLen = ss.rules.length;
+(function( $ ) {
 
-/**
-
-var spotcolor = function (ele, a, p) {
-  var elem = ele, attr = a, pan = p;
-  pt.set_pantone(pan);
-  var newColor = "#" + pt.get_hex();
-  ss.insertRule( elem + " { " + attr + ":" + newColor + ";}", ssLen);
-  ssLen++;
-};
-
-**/
+  var pt = new color();
+  var ss = document.getElementById('panCss').sheet;
+  var ssLen = ss.rules.length;
 
 
-var spotcolor = function (a, p) {
-  var attr = a, pan = p;
-  pt.set_pantone(pan);
-  var newColor = "#" + pt.get_hex();
-  ss.insertRule( $this + " { " + attr + ":" + newColor + ";}", ssLen);
-  ssLen++;
-};
+  $.fn.spotcolor = function (a, p) {
+    var attr = a, pan = p, $el = $(this).selector;
+    pt.set_pantone(pan);
+    var newColor = "#" + pt.get_hex();
+    ss.insertRule( $el + " { " + attr + ":" + newColor + ";}", ssLen);
+    ssLen++;
+  };
+
+})(jQuery);

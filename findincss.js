03d86ce99b8467b45886d23d8b4bf3dbe5f0d38c
coreyzev <coreyzev@zevdesigns.com>
2013-10-18 21:50:55 -0400

    successfully completed CSS interpreting replacement

--- findincss.js
+++ findincss.js
@@ -13,4 +13,29 @@
     ssLen++;
   };
 
+  $.when($.get("main.css"))
+    .done(function(response) {
+        console.log(response);
+    });
+
+//  if (FileReader) {alert("yup");console.log("hola datevid");}
+
+  var rules = String(document.getElementById('pantone-style').innerText);
+//  rules = rules.replace(/\s+/g, '').replace(/}/g, '} ').split(" ");
+//  rules.pop();
+  rules2 = rules.replace(/(:\s+)/g, ':');
+  var pattern = /:([\s\S]*?)(?=;)/g;
+  var result = rules2.match(pattern);
+  var hex=[];
+  for (var i = 0; i < result.length; i++) {
+    if (result[i].length > 1) {
+      result[i] = result[i].substring(1, result[i].length);
+    }
+    var pan = result[i];
+    pt.set_pantone(pan);
+    hex[i] = "#" + pt.get_hex();
+    rules = rules.replace(result[i], hex[i]);
+  };
+  $('#pantone-style').html(rules);
+//  $('#pantone-style').remove();
 })(jQuery);
